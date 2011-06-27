ce90207ce283d83d7ac4e49478cec0d056cabb56
lauwers.kim <lauwers.kim@a7766ecc-fc44-020d-c80f-608928e46cd5>
2011-06-27 08:35:25 +0000

    - Nullpointer fixed - Code clean up

--- PojoMapper/src/main/java/pojo/util/transform/reflect/SelectorInterceptor.java
+++ PojoMapper/src/main/java/pojo/util/transform/reflect/SelectorInterceptor.java
@@ -14,11 +14,13 @@ import static pojo.util.transform.PojoSelector.select;
  * Date: 3/03/11
  */
 public final class SelectorInterceptor implements MethodInterceptor {
-    private Logger logger = LoggerFactory.getLogger(SelectorInterceptor.class);
-    private Object source;
+    private final Logger logger = LoggerFactory.getLogger(SelectorInterceptor.class);
+    private final Object source;
+    private final Class sourceClass;
 
-    public SelectorInterceptor(Object source) {
+    public SelectorInterceptor(final Object source, final Class sourceClass) {
         this.source = source;
+        this.sourceClass = sourceClass;
     }
 
     public Object intercept(final Object o, final Method method, final Object[] objects, final MethodProxy methodProxy) throws Throwable {
@@ -46,7 +48,7 @@ public final class SelectorInterceptor implements MethodInterceptor {
     }
 
     private Object interceptSourceProxyMethods(final SourceProxy sourceProxy, final Method method, final Object[] objects, final MethodProxy methodProxy) throws Throwable {
-        logger.trace("InterceptSourceProxy: returning source of type " + source.getClass().getName());
+        logger.trace("InterceptSourceProxy: returning source of type " + sourceClass.getName());
         return source;
     }
 }
